7662b1b65da4a35ed97f288a095201f381b2fcde
Jeedeexyz <123000633+Jeedeexyz@users.noreply.github.com>
2023-11-14 15:51:57 +0500

    added the remove and place order apis

--- server/routes/Cart.js
+++ server/routes/Cart.js
@@ -7,5 +7,12 @@ const cartController = require("../controllers/Cart");
 router.post("/addToCart", cartController.addToCart);
 //get cartitem by user id
 router.get("/cartitems/:user_id", cartController.getCartItemByUserId);
+//remove the item from cart
+router.delete(
+  "/removeItem/:user_id/:product_id",
+  cartController.deleteCartItemByUserIdAndProductId
+);
+//delete the cart for the user Id
+router.delete("/placeOrder/:user_id", cartController.deleteCartByUserId);
 
 module.exports = router;
